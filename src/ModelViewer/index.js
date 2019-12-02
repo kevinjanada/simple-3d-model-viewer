8291b4c394a8f1eaa197b3a0b959633d842952a3
Kevin Janada <kevin.janada@gmail.com>
2019-12-02 11:10:08 +0700

    add method to raycast objects. add click handler to interact with raycasted object

--- src/ModelViewer/index.js
+++ src/ModelViewer/index.js
@@ -14,15 +14,25 @@ class ModelViewer {
     this.camera = null;
     this.scene = null;
     this.light = null;
-    this.raycaster = null;
     this.renderer = null;
-    this.orbitControls = null;
-    this.mouse = null
     this.modelsDict = {}
-    this.animate = this.animate.bind(this)
-    this.trackMousePosition = this.trackMousePosition.bind(this)
+    this.orbitControls = null;
+    this.raycaster = null;
+    this.mousePosition = {x: 0, y: 0};
+    // The object currently raycasted
+    this.intersectedObject = null;
+    // To pause and start the animation
     this.animationHandle = null
+    
+    // Bind methods context to this
+    this.animate = this.animate.bind(this)
+    this.raycast = this.raycast.bind(this)
+    this.markObject = this.markObject.bind(this)
+    this.getContainerRelativePosition = this.getContainerRelativePosition.bind(this)
+    this.setMousePosition = this.setMousePosition.bind(this)
+    this.clearMousePosition = this.clearMousePosition.bind(this)
 
+    // Setup
     if(domElement) {
       // render to domElement
       this.container = domElement
@@ -35,7 +45,6 @@ class ModelViewer {
       this.containerWidth = window.innerWidth - 20
       this.containerHeight = window.innerHeight - 20
     }
-
     this.camera = new THREE.PerspectiveCamera(70, this.containerWidth / this.containerHeight, 1, 2000)
     this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color(0xf0f0f0)
@@ -46,11 +55,19 @@ class ModelViewer {
     this.renderer.setSize(this.containerWidth, this.containerHeight)
     this.container.appendChild(this.renderer.domElement)
     this.orbitControls = initializeOrbitControls(this.camera, this.renderer)
-    this.mouse = new THREE.Vector2()
-    document.addEventListener('mousemove', this.trackMousePosition, false)
+    this.raycaster = new THREE.Raycaster()
+
+    // Event Listeners
+    document.addEventListener('click', this.markObject)
+    document.addEventListener('mousemove', this.setMousePosition)
+    document.addEventListener('mouseout', this.clearMousePosition)
+    document.addEventListener('mouseleave', this.clearMousePosition)
+    this.container.addEventListener('mouseenter', this.onMouseEnterCanvas) // TODO: Implement this
+    this.container.addEventListener('mouseleave', this.onMouseLeaveCanvas) // TODO: Implement this
   }
 
   render () {
+    this.raycast()
     this.renderer.render(this.scene, this.camera)
   }
 
@@ -60,14 +77,65 @@ class ModelViewer {
     this.orbitControls.update()
   }
 
-  trackMousePosition(event) {
-    event.preventDefault()
-    this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1
-    this.mouse.y = (event.clientY / window.innerHeight) * 2 - 1
+  /**
+   * Get the x and y of canvas element
+   * @param {DOMEvent object} event 
+   */
+  getContainerRelativePosition(event) {
+    const rect = this.container.getBoundingClientRect()
+    return {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top
+    }
   }
 
-  centerToObject(modelKey, offset) {
-    const boundingBox = new THREE.Box3()
+  /**
+   * Set the mouse position relative to the canvas / container of renderer
+   * @param {DOMEvent object} event 
+   * this.mousePosition.x == -1 <= x <= 1 
+   * this.mousePosition.y == -1 <= y <= 1
+   */
+  setMousePosition(event) {
+    const pos = this.getContainerRelativePosition(event);
+    this.mousePosition.x = (pos.x / this.container.clientWidth ) *  2 - 1;
+    this.mousePosition.y = (pos.y / this.container.clientHeight) * -2 + 1;  // note we flip Y
+  }
+
+  /**
+   * unlike the mouse which always has a position
+   * if the user stops touching the screen we want
+   * to stop raycasting. For now we just pick a value
+   * unlikely to pick something
+   */
+  clearMousePosition() {
+    this.mousePosition.x = -100000;
+    this.mousePosition.y = -100000;
+  }
+
+  /**
+   * Cast ray from mouse position to detect closest object at mouse position
+   */
+  raycast() {
+     // cast a ray through the frustum
+    this.raycaster.setFromCamera(this.mousePosition, this.camera);
+    // get the list of objects the ray intersected
+    const intersectedObjects = this.raycaster.intersectObjects(this.scene.children, true);
+    if (intersectedObjects.length > 0) {
+      // pick the first object. It's the closest one
+      this.intersectedObject = intersectedObjects[0].object;
+      // save its color
+      // this.pickedObjectSavedColor = this.pickedObject.material.emissive.getHex();
+      // set its emissive color to flashing red/yellow
+      // this.pickedObject.material.emissive.setHex(0xFF0000);
+    } else {
+      this.intersectedObject = null;
+    }
+  }
+
+  markObject() {
+    if (this.intersectedObject) {
+      this.intersectedObject.material.emissive.setHex(0xFF0000);
+    }
   }
 
   centerCameraToObject(modelKey, offset) {
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import modelResetOrigin from './models/3D/EXPANDER_EXPELLER_Line02.nwd/DWS\ step
 
 const modelViewer = new ModelViewer()
 const main = async () => {
-  // await modelViewer.loadFBX(testFBXModel, 'testModel')
   await modelViewer.loadFBX(modelResetOrigin, 'tanks')
   modelViewer.animate()
 }
